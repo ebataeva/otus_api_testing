3a68d2371710eeaa9fc6d0bca191683c5b346e71
Sukharev <S.Sukharev@nexign.com>
2022-04-26 18:06:37 +0300

    initial

---
+++ support.py
@@ -0,0 +1,31 @@
+from jsonschema import validate
+import os
+import json
+
+from typing import Dict
+
+DIRPATH = os.path.abspath(os.path.dirname(__file__))
+HOUND = [
+    "afghan",
+    "basset",
+    "blood",
+    "english",
+    "ibizan",
+    "plott",
+    "walker"
+]
+BULLDOGS = [
+    "boston",
+    "english",
+    "french"
+]
+
+
+def assert_valid_schema(data, schema_file):
+    with open(DIRPATH + '/' + schema_file, encoding='utf-8') as f:
+        schema = json.load(f)
+    return validate(instance=data, schema=schema)
+
+
+def check_api_json_schema(request, schema):
+    return validate(instance=request, schema=schema)
